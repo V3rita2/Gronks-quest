c45c01dbf6b1121bd843d1d2934cd5c859cdb5a2
Veritas <rose.daemon.belmont@gmail.com>
2021-06-26 15:05:33 -0700

    adds descriptions for poi adds function to describe things

--- js/script.js
+++ js/script.js
@@ -19,22 +19,40 @@ let pants = true;
 let statue = "unbroken";
 
 let room = 0;
+
+const description = {
+    statue: ("statue made of stone. Holding shiny key in one hand. Other hand look like when Gork take thing from Gronk"),
+    door: ("door is big and heavy. has hole in door near handle.")
+};
 //function to append new room description to playSpace
 
 function gronkSpeaks(gronkEyes){
     let speak = document.createElement("span");
     let pause = document.createElement("br");
     let pause2 = document.createElement("br");
-
+    
+    
     speak.innerText = (gronkEyes);
     play.appendChild(speak);
     play.appendChild(pause);
     play.appendChild(pause2);
 };
-
+//function to append description of important objects in the room
+function gronkDescribes(item) {
+    let speak = document.createElement("span");
+    let pause = document.createElement("br");
+    let pause2 = document.createElement("br");
+    let words = (description[item]);
+    
+    
+    speak.innerText = (words);
+    play.appendChild(speak);
+    play.appendChild(pause);
+    play.appendChild(pause2);
+};
 //first room object
 const room0 = {
-    gronkEyes: ("Hole is big and deep and smooth, Gronk can't climb out. Big statue near Gronk holding shiny key, other hand look like Gork when taking something from Gronk. Door away from Gronk on wall."),
+    gronkEyes: ("Hole is big and deep and smooth, Gronk can't climb out. Big statue near Gronk with shiny hand. Door away from Gronk on wall."),
 
     POI: ("statue", "door"),
 
@@ -47,15 +65,17 @@ $("#inputLine").submit(gronkDo);
 
 //Gronk's brain
 function gronkDo(e) {
+    let declaration = $("input").first().val();
+    let item = $("input").last().val();
     //checks if player is picking something up, and puts it in their inventory
-    if( $("input").first().val() === "pick") {
-        pockets.push($("input").last().val());
+    if( declaration === "pick") {
+        pockets.push(item);
     }
-    else if($("input").first().val() === "go") {
+    else if(declaration === "go") {
         switch (room) {
             case 0:
-                if ($("input").last().val() !== "door" || $("input").last().val() !== "statue") {
-                    //DO THIS NEXT IDIOT
+                if (item !== "door" || item !== "statue") {
+                    //DO THIS NEXT
                 }
         }
             
